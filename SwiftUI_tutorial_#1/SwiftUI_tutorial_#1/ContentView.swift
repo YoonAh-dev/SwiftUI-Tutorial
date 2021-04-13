97baa6b79b81e50e09e04870f1fbeaade9c1c778
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-04-13 13:46:22 +0900

    🍎 레포 초기 세팅

---
+++ SwiftUI_tutorial_#1/SwiftUI_tutorial_#1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorial_#1
+//
+//  Created by SHIN YOON AH on 2021/04/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!123")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
