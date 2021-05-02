01a91cc0035fe46303461877979280544a299541
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-02 11:11:34 +0900

    [Add] 2nd example 추가

---
+++ SwiftUI_Picker_tutorial/SwiftUI_Picker_tutorial/SecondExampleView.swift
@@ -0,0 +1,88 @@
+//
+//  SecondExampleView.swift
+//  SwiftUI_Picker_tutorial
+//
+//  Created by SHIN YOON AH on 2021/05/01.
+//
+
+import SwiftUI
+
+enum School: String, CaseIterable {
+    case elementary = "초등학교"
+    case middle = "중학교"
+    case high = "고등학교"
+}
+
+struct MyFriend: Identifiable, Hashable {
+    var id = UUID()
+    var name: String
+    var school: School
+}
+
+func prepareData() -> [MyFriend] {
+    var newList = [MyFriend]()
+    
+    for i in 0...20 {
+        let newFriend = MyFriend(name: "내 친구 \(i)", school: School.allCases.randomElement()!)
+        newList.append(newFriend)
+    }
+    
+    return newList
+}
+
+struct SecondExampleView: View {
+    @State private var schoolFilter: School = .elementary
+    @State private var myFriendsList: [MyFriend] = []
+    
+    // 생성자 메소드
+    init() {
+        _myFriendsList = State(initialValue: prepareData())
+    }
+    
+    var body: some View {
+        VStack {
+            Text("filteredValue: \(schoolFilter.rawValue)")
+            
+            List() {
+                Section(header: Text("언제 동창?"), content: {
+                    HStack {
+                        Spacer()
+                        Picker(selection: $schoolFilter,
+                               label: Text(""),
+                               content: {
+                                Text("초등학교").tag(School.elementary)
+                                Text("중학교").tag(School.middle)
+                                Text("고등학교").tag(School.high)
+                        }).pickerStyle(SegmentedPickerStyle())
+                            .frame(width: 250)
+                        Spacer()
+                    }
+                })
+                
+                Section(header: Text(""), content: {
+                    ForEach(myFriendsList.filter {
+                        $0.school == schoolFilter
+                    }) { friendItem in
+                            GeometryReader { geo in
+                                HStack {
+                                    Text("name: \(friendItem.name)")
+                                        .frame(width: geo.size.width / 2)
+                                    Divider()
+                                    
+                                    Text("school: \(friendItem.school.rawValue)")
+                                        .frame(width: geo.size.width / 2)
+                                        
+                                }
+                            }
+                        }
+                })
+            }
+        }
+    }
+}
+
+struct SecondExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondExampleView()
+    }
+}
