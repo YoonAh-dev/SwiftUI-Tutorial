a06c8ee2280c9a22a23a187a9a640ef28a5690aa
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-03 15:23:33 +0900

    [Add] 메뉴 example

---
+++ SwiftUI_Menu_tutorial/SwiftUI_Menu_tutorial/SwiftUI_Menu_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Menu_tutorialApp.swift
+//  SwiftUI_Menu_tutorial
+//
+//  Created by SHIN YOON AH on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Menu_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
