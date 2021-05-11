4735dd2a371224f6ab07dc1b53e0b50e727e5289
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-11 11:28:40 +0900

    [FEAT] RefreshControl 추가

--- SwiftUI_Alamofire_tutorial/SwiftUI_Alamofire_tutorial/ContentView.swift
+++ SwiftUI_Alamofire_tutorial/SwiftUI_Alamofire_tutorial/ContentView.swift
@@ -6,20 +6,65 @@
 //
 
 import SwiftUI
+import UIKit
+import Introspect
+
+class RefreshControlHelper {
+    // MARK: - Properties
+    var parentContentView: ContentView?
+    var refreshControl: UIRefreshControl?
+    
+    @objc
+    func didRefresh() {
+        print(#fileID, #function, #line, "")
+        guard let parentContentView = parentContentView,
+              let refreshControl = refreshControl else {
+            print("parentContentView, refreshControl 가 nil입니다")
+            return
+        }
+        
+        // UI화면 thread에서 돌린다.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7, execute:  {
+            print("refresh completed")
+            // MARK: - TODO : API 다시 땡기기
+//            parentContentView.randomUserViewModel.fetchRandomUsers()
+            // MARK: - Combine으로 땡겨 쓰기
+            // 이벤트가 들어가는 시점
+            parentContentView.randomUserViewModel.refreshActionSubject.send()
+            refreshControl.endRefreshing()
+        })
+    }
+}
 
 struct ContentView: View {
-    
     @ObservedObject var randomUserViewModel = RandomUserViewModel()
+    
+    let refreshControlHelper = RefreshControlHelper()
     
     var body: some View {
         // 각각의 아이템들을 ForEach문처럼 가져와서 뷰를 그림
         List(randomUserViewModel.randomUsers) { randomUser in
             CellView(randomUser)
-        }
-        
-//        List(0...100, id: \.self) { id in
-//            CellView(imageUrl: url)
+         }
+        .introspectTableView{ self.configureRefreshControl($0) }
+//        .introspectTableView { tableView in
+//            self.configureRefreshControl(tableView)
 //        }
+    }
+}
+
+// MARK: - Helper Methods
+extension ContentView {
+    fileprivate func configureRefreshControl(_ tableView: UITableView) {
+        print(#fileID, #function, #line, "")
+        // 이 안에 있는 건 UIKit
+        let myRefreshControl = UIRefreshControl()
+        myRefreshControl.tintColor = .systemBlue
+        refreshControlHelper.refreshControl = myRefreshControl
+        refreshControlHelper.parentContentView = self
+        myRefreshControl.addTarget(refreshControlHelper, action: #selector(RefreshControlHelper.didRefresh), for: .valueChanged)
+        
+        tableView.refreshControl = myRefreshControl
     }
 }
 
--- SwiftUI_Alamofire_tutorial/SwiftUI_Alamofire_tutorial/Data/RandomUserViewModel.swift
+++ SwiftUI_Alamofire_tutorial/SwiftUI_Alamofire_tutorial/Data/RandomUserViewModel.swift
@@ -17,14 +17,26 @@ class RandomUserViewModel: ObservableObject {
     // @Published 되어 있어서 이벤트로 인해서 변경되는 상황을 알 수 있음
     @Published var randomUsers = [RandomUser]()
     
+    // 단방향으로 이벤트를 보냄
+    // 보내는 데이터가 없기 때문에 () 이렇게 생성
+    var refreshActionSubject = PassthroughSubject<(), Never>()
+    
     var baseUrl = "https://randomuser.me/api/?results=100"
     
     init() {
         print(#fileID, #function, #line, "")
         fetchRandomUsers()
+        
+        refreshActionSubject.sink { [weak self] _ in
+            guard let self = self else { return }
+            print("RandomUserViewModel - init - refreshActionSubject")
+            self.fetchRandomUsers()
+        }.store(in: &subscription)
     }
     
-    func fetchRandomUsers() {
+    // 외부에서 접근 불가능
+    // 하지만 refreshActionSubject에서 api땡기기 가능
+    fileprivate func fetchRandomUsers() {
         print(#fileID, #function, #line, "")
         AF.request(baseUrl)
             .publishDecodable(type: RandomUserResponse.self)
