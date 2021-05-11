051d1de2473dfd24ed6ea133a258da003fb7ce3c
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-11 16:12:51 +0900

    [ADD] Darkmode 지원 추가

---
+++ SwiftUI_Darkmode_tutorial/SwiftUI_Darkmode_tutorial/SwiftUI_Darkmode_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Darkmode_tutorialApp.swift
+//  SwiftUI_Darkmode_tutorial
+//
+//  Created by SHIN YOON AH on 2021/05/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Darkmode_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
